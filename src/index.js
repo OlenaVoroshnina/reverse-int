f4e977779e9391c1a8b5d8ccc744036767c0488f
OlenaVoroshnina <113518916+OlenaVoroshnina@users.noreply.github.com>
2023-09-03 23:55:28 +0200

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const str = Math.abs(n).toString();
+    const reversedStr = str.split("").reverse().join("");
+    const reversedNumber = parseInt(reversedStr);
+    return reversedNumber;
+};
